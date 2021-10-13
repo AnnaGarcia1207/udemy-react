e24a2474a905a853a511a520ff703934eabd961d
Anna Garcia <anna.garcia.18@cnu.edu>
2021-10-13 09:47:33 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Import the react and the ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Create a react component
+const App = () => {
+    return <div>Hi There!</div>
+} 
+
+//can also be written as:
+// const App = function() {
+//     return <div> Hi there!</div>
+// }
+
+// Take the react componenet and show it on the screen
+ReactDOM.render (
+    <App />, // calls the function that we just created above
+    document.querySelector('#root') // Calls the index.html 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (module.hot) {
+    module.hot.accept();
+}
