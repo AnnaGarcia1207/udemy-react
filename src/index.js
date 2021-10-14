38b4cf7ab0c413b834e8b8e1588d4cd18c7dd578
Anna Garcia <anna.garcia.18@cnu.edu>
2021-10-14 10:00:52 -0400

    JSX differences. see examples

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function getButtonText() {
+    return 'Function call';
+}
+
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>
+    // const buttonText = 'Click Me!';
+    const buttonText = {text: 'Click Me!!'};
+    const labelText = 'Enter name:';
+
+    return (
+    <div>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text"/>
+        {/* This is HTML v */}
+        {/* <button style="background-color: blue; color: white;">Submit</button>  */} 
+        {/* This is JSX convertion from HTML */}
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>
+            {/* Submit - We should replace with buttonText variable */}
+            {/* {buttonText} - Example of a variable call */}
+            {buttonText.text}
+            {/* {getButtonText()} - Example of a function call */}
+            </button>
+  </div>
+    );
 } 
 
 //can also be written as:
